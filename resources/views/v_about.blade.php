fee1bbee42ffbf5e0715a715eaad1279b171fd23
Anthony Rafael <64669631+Anthony-Citizen@users.noreply.github.com>
2021-11-22 09:08:31 +0700

    Initial commit

---
+++ resources/views/v_about.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Expert Auto Care - Home</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('user_template')}}/assets/img/favicon.ico" />
+
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="{{asset('user_template')}}/assets/css/bootstrap-5.0.0-alpha-2.min.css" />
+    <link rel="stylesheet" href="{{asset('user_template')}}/assets/css/LineIcons.2.0.css" />
+    <link rel="stylesheet" href="{{asset('user_template')}}/assets/css/animate.css" />
+    <link rel="stylesheet" href="{{asset('user_template')}}/assets/css/main.css" />
+    <link rel="stylesheet" href="{{asset('user_template')}}/assets/css/profile.css" />
+</head>
+
+<body>
+    <!-- ========================= Header ========================= -->
+    <header class="header">
+        <div class="navbar-area">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12">
+                        <nav class="navbar navbar-expand-lg">
+                            <a class="navbar-brand" href="index.html">
+                                <img src="{{asset('user_template')}}/assets/img/logo/logo.png" alt="Logo" />
+                            </a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="toggler-icon"></span>
+                                <span class="toggler-icon"></span>
+                                <span class="toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                                <ul id="nav" class="navbar-nav ml-auto">
+                                    <li class="nav-item">
+                                        <a href="/">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="active" href="/about">About Us</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="/tutorial">How it Works</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="/history">History</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- navbar collapse -->
+                        </nav>
+                        <!-- navbar -->
+                    </div>
+                </div>
+                <!-- row -->
+            </div>
+            <!-- container -->
+        </div>
+        <!-- navbar area -->
+    </header>
+    <!-- ========================= header end ========================= -->
+
+
+    <section class="hero-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-sm-8 col-lg-6">
+                    <!-- Section Heading-->
+                    <div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s"
+                        style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                        <h3>Our Creative <span> Team</span></h3>
+                        <div class="line"></div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-center text-center">
+                <!-- Single Advisor-->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.3s"
+                        style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
+                        <!-- Team Thumb-->
+                        <div class="advisor_thumb"><img class="ct"
+                                src="https://bootdey.com/img/Content/avatar/avatar7.png" style="height:100%" alt="">
+                            <!-- Social Info-->
+                            <div class="social-info"><a href="https://www.facebook.com/anthony.rafael.547389"
+                                    target="_blank"><i class="fa fa-facebook"></i></a><a
+                                    href="https://twitter.com/AnthonyR2503?s=08" target="_blank"><i
+                                        class="fa fa-twitter"></i></a><a
+                                    href="https://www.linkedin.com/in/anthony-rafael-2503/" target="_blank"><i
+                                        class="fa fa-linkedin"></i></a></div>
+                        </div>
+                        <!-- Team Details-->
+                        <div class="single_advisor_details_info">
+                            <h6>Anthony Rafael</h6>
+                            <p class="designation">Programmer</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Advisor-->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s"
+                        style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                        <!-- Team Thumb-->
+                        <div class="advisor_thumb" style="background-color: #fbb238;"><img class="ct"
+                                src="https://bootdey.com/img/Content/avatar/avatar1.png" style="height:100%" alt="">
+                            <!-- Social Info-->
+                            <div class="social-info"><a href="https://www.facebook.com/gerald.nicholas.12/"
+                                    target="_blank"><i class="fa fa-facebook"></i></a><a
+                                    href="https://twitter.com/Boolaakk?s=09" target="_blank"><i
+                                        class="fa fa-twitter"></i></a><a
+                                    href="https://www.linkedin.com/in/gerald-nicholas-b13413202/" target="_blank"><i
+                                        class="fa fa-linkedin"></i></a></div>
+                        </div>
+                        <!-- Team Details-->
+                        <div class="single_advisor_details_info">
+                            <h6>Vincent Christopher</h6>
+                            <p class="designation">Programmer</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Advisor-->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.4s"
+                        style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                        <!-- Team Thumb-->
+                        <div class="advisor_thumb" style="background-color: #b0dce4;"><img class="ct"
+                                src="https://bootdey.com/img/Content/avatar/avatar6.png" style="height:100%" alt="">
+                            <!-- Social Info-->
+                            <div class="social-info"><a href="https://www.facebook.com/m.zidane.528" target="_blank"><i
+                                        class="fa fa-facebook"></i></a><a href="https://mobile.twitter.com/@mzidanef8"
+                                    target="_blank"><i class="fa fa-twitter"></i></a><a
+                                    href="https://www.linkedin.com/mwlite/in/m-zidane-0a7417202" target="_blank"><i
+                                        class="fa fa-linkedin"></i></a></div>
+                        </div>
+                        <!-- Team Details-->
+                        <div class="single_advisor_details_info">
+                            <h6>Wahyu Koco</h6>
+                            <p class="designation">Programmer</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+
+    <!-- ========================= footer start ========================= -->
+    <footer id="footer" class="footer">
+        <div class="copy-right-wrapper">
+            <p>Expert System Project Development</p>
+        </div>
+
+
+    </footer>
+
+
+
+
+    <!-- ========================= footer end ========================= -->
+
+    <!-- ========================= scroll-top ========================= -->
+    <a href="#" class="scroll-top btn-hover">
+        <i class="lni lni-chevron-up"></i>
+    </a>
+
+    <!-- ========================= JS here ========================= -->
+    <script src="{{asset('user_template')}}/assets/js/bootstrap.5.0.0.alpha-2-min.js"></script>
+    <script src="{{asset('user_template')}}/assets/js/count-up.min.js"></script>
+    <script src="{{asset('user_template')}}/assets/js/wow.min.js"></script>
+    <script src="{{asset('user_template')}}/assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ resources/views/v_history.blade.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<title>Expert Auto Care - Home</title>
+	<meta name="description" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="shortcut icon" type="image/x-icon" href="{{asset('user_template')}}/assets/img/favicon.ico" />
+
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+
+	<link rel="stylesheet" href="{{asset('user_template')}}/assets/css/bootstrap-5.0.0-alpha-2.min.css" />
+	<link rel="stylesheet" href="{{asset('user_template')}}/assets/css/LineIcons.2.0.css" />
+	<link rel="stylesheet" href="{{asset('user_template')}}/assets/css/animate.css" />
+	<link rel="stylesheet" href="{{asset('user_template')}}/assets/css/main.css" />
+	<link rel="stylesheet" href="{{asset('user_template')}}/assets/css/history.css" />
+</head>
+
+<body>
+	<!-- ========================= Header ========================= -->
+	<header class="header">
+		<div class="navbar-area">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-12">
+						<nav class="navbar navbar-expand-lg">
+							<a class="navbar-brand" href="index.html">
+								<img src="{{asset('user_template')}}/assets/img/logo/logo.png" alt="Logo" />
+							</a>
+							<button class="navbar-toggler" type="button" data-toggle="collapse"
+								data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+								aria-expanded="false" aria-label="Toggle navigation">
+								<span class="toggler-icon"></span>
+								<span class="toggler-icon"></span>
+								<span class="toggler-icon"></span>
+							</button>
+
+							<div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+								<ul id="nav" class="navbar-nav ml-auto">
+									<li class="nav-item">
+										<a href="/">Home</a>
+									</li>
+									<li class="nav-item">
+										<a href="/about">About Us</a>
+									</li>
+									<li class="nav-item">
+										<a href="/tutorial">How it Works</a>
+									</li>
+									<li class="nav-item">
+										<a class="active" href="/history">History</a>
+									</li>
+								</ul>
+							</div>
+							<!-- navbar collapse -->
+						</nav>
+						<!-- navbar -->
+					</div>
+				</div>
+				<!-- row -->
+			</div>
+			<!-- container -->
+		</div>
+		<!-- navbar area -->
+	</header>
+	<!-- ========================= header end ========================= -->
+
+
+	<section class="timeline_area section_padding_130">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-12 col-sm-8 col-lg-6">
+					<!-- Section Heading-->
+					<div class="section_heading text-center">
+						<h6>Our History</h6>
+						<h3>A brief stories of our 2 years company journey</h3>
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<!-- Timeline Area-->
+					<div class="apland-timeline-area">
+						<!-- Single Timeline Content-->
+						<div class="single-timeline-area">
+							<div class="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInLeft;">
+								<p>Week 6</p>
+							</div>
+							<div class="row">
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.7s" style="visibility: visible; animation-delay: 0.7s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-address-book" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>User Testing</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-archive" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>Fixed bug</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Single Timeline Content-->
+						<div class="single-timeline-area">
+							<div class="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInLeft;">
+								<p>Week 5</p>
+							</div>
+							<div class="row">
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-briefcase" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>Cont. Developing</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-desktop" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>Fixed bug</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Single Timeline Content-->
+						<div class="single-timeline-area">
+							<div class="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInLeft;">
+								<p>Week 4</p>
+							</div>
+							<div class="row">
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-id-card" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>Knowledge Representation</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-desktop" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>Inference Engine</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.7s" style="visibility: visible; animation-delay: 0.7s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-picture-o" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>Documentation</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Single Timeline Content-->
+						<div class="single-timeline-area">
+							<div class="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInLeft;">
+								<p>Week 3</p>
+							</div>
+							<div class="row">
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-id-card" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>Knowledge Acquisition</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-desktop" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>Interface Design</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Single Timeline Content-->
+						<div class="single-timeline-area">
+							<div class="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInLeft;">
+								<p>Week 2</p>
+							</div>
+							<div class="row">
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-id-card" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>Knowledge Acquisition</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Single Timeline Content-->
+						<div class="single-timeline-area">
+							<div class="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInLeft;">
+								<p>Week 1</p>
+							</div>
+							<div class="row">
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+										<div class="timeline-icon"><i class="fa fa-id-card" aria-hidden="true"></i></div>
+										<div class="timeline-text">
+											<h6>Observation</h6>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- ========================= footer start ========================= -->
+	<footer id="footer" class="footer">
+		<div class="copy-right-wrapper">
+			<p>Expert System Project Development</p>
+		</div>
+
+
+	</footer>
+
+
+
+
+	<!-- ========================= footer end ========================= -->
+
+	<!-- ========================= scroll-top ========================= -->
+	<a href="#" class="scroll-top btn-hover">
+		<i class="lni lni-chevron-up"></i>
+	</a>
+
+	<!-- ========================= JS here ========================= -->
+	<script src="{{asset('user_template')}}/assets/js/bootstrap.5.0.0.alpha-2-min.js"></script>
+	<script src="{{asset('user_template')}}/assets/js/count-up.min.js"></script>
+	<script src="{{asset('user_template')}}/assets/js/wow.min.js"></script>
+	<script src="{{asset('user_template')}}/assets/js/main.js"></script>
+</body>
+
+</html>
